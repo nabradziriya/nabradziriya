97be1f7efccc96acd39442435069959bfa680022
nabradziriya <abdelnamich@gmail.com>
2026-09-06 12:30:22 +0100

    Add files via upload

---
+++ 3/public/firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase Web configuration for the Sober Path app.
+export const firebaseConfig = {
+  apiKey: "AIzaSyA45tj2cyarTZMHVt1X6j7r9SWvnGIk2Mw",
+  authDomain: "sober-7a9ac.firebaseapp.com",
+  projectId: "sober-7a9ac",
+  storageBucket: "sober-7a9ac.firebasestorage.app",
+  messagingSenderId: "430700347441",
+  appId: "1:430700347441:web:054768eeb8ca5532229510"
+};
